bd3b141138b44454a37b2b9df61907c1cbb19055
Goferov <marcingodfryd@gmail.com>
2024-04-06 20:54:13 +0200

    Create Vector3D class

--- src/Vector3D.php
+++ src/Vector3D.php
@@ -1,2 +1,31 @@
 <?php
 
+class Vector3D {
+
+    private float $z;
+    public function __construct(float $x, float $y, float $z) {
+        parent::__contruct($x, $y);
+        $this->z = $z;
+    }
+
+    public function abs(): float {
+        return 1;
+    }
+
+    public function dot(Vector $v): float {
+        $coords = $v->getCoords();
+        return $this->x * $coords['x'] + $this->y * $coords['y'] + $this->z * $coords['z'];
+    }
+
+    public function vec(Vector $v): Vector {
+        return $v;
+    }
+
+    public function getCoords(): array {
+        return [
+            'x' => $this->x,
+            'y' => $this->y,
+            'z' => $this->z,
+        ];
+    }
+}
